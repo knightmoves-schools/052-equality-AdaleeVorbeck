42b023d9a07e4ee68f2c43d031c8d34d5c5bf3fb
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-06 20:17:59 +0000

    TILL THE LAST OF THE BUGS

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function priceCheck(price) {
+    if (price == 1.50) {
+        return 'match';
+    }
+}
 
+function strictPriceCheck(price) {
+    if (price === 1.50) {
+        return 'match';
+    }
+}
+
+
+//should not return match when 1.00 is passed in as an argument to priceCheck
+
+//should not return match when the string 1.50 is passed in as an argument to strictPriceCheck
+//should not return match when 1.00 is passed in as an argument to strictPriceCheck
